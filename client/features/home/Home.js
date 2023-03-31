dc45a1a5186fbfbb7679c5282951ad3ad2c141c1
Steven Scheck <111304697+stevenscheck@users.noreply.github.com>
2023-03-31 09:34:35 -0700

    modify boiler

--- client/features/home/Home.js
+++ client/features/home/Home.js
@@ -5,11 +5,9 @@ import { useSelector } from 'react-redux';
  * COMPONENT
  */
 const Home = (props) => {
-  const username = useSelector((state) => state.auth.me.username);
-
   return (
     <div>
-      <h3>Welcome, {username}</h3>
+      <h3>Welcome</h3>
     </div>
   );
 };
